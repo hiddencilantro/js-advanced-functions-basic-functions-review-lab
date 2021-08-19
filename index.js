fde6ca2b7dbcf05fb02c74370e6c23b3e0219017
hiddencilantro <stevekim1109@gmail.com>
2021-08-19 10:56:31 -0400

    commit tree 9e67d733f3bb1dfe9376254361aa7f948bf4fe3e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(string="go to the office") {
+    return `This Monday, I will ${string}.`;
+}
+
+function wrapAdjective(flair="*") {
+    return function(param="special") {
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: () => 1+3,
+    subtract: () => 1-3,
+    multiply: () => 1*3,
+    divide: () => 10/5
+};
+
+function actionApplyer(int, arr) {
+    return arr.reduce((accum, element) => {return element(accum)}, int);
+}
